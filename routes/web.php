e2ac1952f70f837af0171940835828987c123274
Unknown <clementserrano3@gmail.com>
2017-03-15 10:50:15 +0100

    Project ready to do

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Afficher le formulaire d'authentification 
+
+// Réponse au clic sur le bouton Valider du formulaire formLogin
+
+// Déloguer le propriétaire
+
+
+// Afficher la liste des Oeuvres
+
+// Afficher un manga pour pouvoir le modifier
+
+// Enregistrer la mise à jour d'une oeuvre
+
+// Afficher le formulaire de saisie d'une nouvelle oeuvre
+
+// Supprimer une oeuvre
+
+
+// Afficher la liste des réservations
+
+// Réserver une oeuvre
+
+// Valider une réservation
+
+// Confirmer une réservation
+
+// Supprimer une réservation
+
+
